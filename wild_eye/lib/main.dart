cb362609f376cc3812adfad2f414c3ae5d27ec8e
Amelia Jones <jonesas@hendrix.edu>
2024-09-24 18:24:42 -0500

    buttons

--- wild_eye/lib/main.dart
+++ wild_eye/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wild_eye/objects/flora.dart';
+import 'package:wild_eye/objects/fauna.dart';
+import 'package:wild_eye/widgets/fauna_list.dart';
+import 'package:wild_eye/widgets/flora_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   // This widget is the home page of your application
@@ -31,42 +36,84 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class FloraList extends StatefulWidget {
+  const FloraList({super.key});
+
+  @override
+  State<FloraList> createState() => _FloraListState();
+}
+
+class _FloraListState extends State<FloraList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(mainAxisSize: MainAxisSize.max, children: [
+      SingleChildScrollView(child: Text('flora'),),
+    ],);
+  }
+}
+class FaunaList extends StatefulWidget {
+  const FaunaList({super.key});
+
+  @override
+  State<FaunaList> createState() => _FaunaListState();
+}
+
+class _FaunaListState extends State<FaunaList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(mainAxisSize: MainAxisSize.max, children: [
+      SingleChildScrollView(child: Text('fauna'),),
+    ],);
+  }
+}
+
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(onPressed: () {},
+                child: const Icon(Icons.location_on)),
+                ElevatedButton(onPressed: (){},
+                child: const Icon(Icons.add),),
+              ]
+            );
+  
+    
+  }
+}
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  //final List<flora> floralist;
+  //final _floraSet = <flora>{};
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Wild-Eye')
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            FloraList(),
+            FaunaList(),
+            ButtonSection(),
+            ],
         ),
+       
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      
     );
   }
+
 }
