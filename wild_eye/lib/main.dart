513b166de4e2f064a2806adf32ee1eb95a1a3615
Amelia Jones <jonesas@hendrix.edu>
2024-09-25 12:48:58 -0500

    removed homescreen from main file?

--- wild_eye/lib/main.dart
+++ wild_eye/lib/main.dart
@@ -1,114 +1,25 @@
+import 'dart:async';
+import 'package:wild_eye/pages/HomeScreen.dart';
+
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
-}
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WildEye',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 25, 140, 29)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Home'),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
 }
 
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  // This widget is the home page of your application
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class FloraList extends StatefulWidget {
-  const FloraList({super.key});
-
-  @override
-  State<FloraList> createState() => _FloraListState();
-}
-
-class _FloraListState extends State<FloraList> {
-  @override
-  Widget build(BuildContext context) {
-    return const Column(mainAxisSize: MainAxisSize.max, children: [
-      SingleChildScrollView(child: Text('flora'),),
-    ],);
-  }
-}
-class FaunaList extends StatefulWidget {
-  const FaunaList({super.key});
-
-  @override
-  State<FaunaList> createState() => _FaunaListState();
-}
-
-class _FaunaListState extends State<FaunaList> {
-  @override
-  Widget build(BuildContext context) {
-    return const Column(mainAxisSize: MainAxisSize.max, children: [
-      SingleChildScrollView(child: Text('fauna'),),
-    ],);
-  }
-}
 
 
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(onPressed: () {},
-                child: const Icon(Icons.location_on)),
-                ElevatedButton(onPressed: (){},
-                child: const Icon(Icons.add),),
-              ]
-            );
-  
-    
-  }
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  //final List<flora> floralist;
-  //final _floraSet = <flora>{};
-  
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Wild-Eye')
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            FloraList(),
-            FaunaList(),
-            ButtonSection(),
-            ],
-        ),
-       
-      ),
-      
-    );
-  }
 
-}
+
+
+
