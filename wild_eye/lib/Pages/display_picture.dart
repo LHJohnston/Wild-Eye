79413da764d3d8c361da04f2790ce2b0595bc230
Leigh <johnstonlh@hendrix.edu>
2024-09-25 12:41:53 -0500

    added camera pages

---
+++ wild_eye/lib/Pages/display_picture.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
