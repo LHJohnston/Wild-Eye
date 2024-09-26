fe1333fe84205a99816ab4017a9c559bb2e69a04
Amelia Jones <jonesas@hendrix.edu>
2024-09-26 14:04:41 -0500

    map

---
+++ wild_eye/lib/Pages/map_display.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class MapDisplay extends StatefulWidget {
+  const MapDisplay({super.key});
+
+  @override
+  State<MapDisplay> createState() => _MapDisplayState();
+}
+
+class _MapDisplayState extends State<MapDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
